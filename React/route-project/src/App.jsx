4667920e7bf5b7b50c7ccfaedd493247d14c1ac7
SiriSamala <sirisamala09@gmail.com>
2024-10-27 17:34:24 +0530

    Day6(route-project)

--- React/route-project/src/App.jsx
+++ React/route-project/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Products from './pages/Products'
 const App = () => {
   return (
-    <div>App hello rfrergt</div>
+    <>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/contact' element={<Contact />} />
+                
+            </Routes>
+        </BrowserRouter>
+    </>
   )
 }
 

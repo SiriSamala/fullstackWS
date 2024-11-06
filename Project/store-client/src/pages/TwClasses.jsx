81441fc3c295e9efd0e6b33538f05ac0f47a8fbb
SiriSamala <sirisamala09@gmail.com>
2024-11-06 12:44:54 +0530

    Day11(bcrypt)

---
+++ Project/store-client/src/pages/TwClasses.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const TwClasses = () => {
+    // h-92 : tw predefined height (24rem)
+    // h-screen : height : 100vh
+    // h- : height : 
+    // h- : height : 
+    // h- : height : 
+    // h- : height : 
+    // h- : height : 
+    // h-full : height : 100%
+    // bg-red-500 : background color
+    // text-white : text color
+    // text-3xl : text size
+    // font-bold : font change
+    // gap-2 : gap: 0.25rem
+    // border-2 : border for text
+    // rounded-md : border curve
+    // p-5 : paddling
+    // pt : top padding
+    // px : left and right padding
+    // py :top and bottom padding
+    // border-pink-500 : border color
+    // shadow-2xl : shadow
+    // shadow-pink_500 : color shadow
+    // shadow-md : medium shadow
+    // hover:bg-pink-500 : hover(when cursor goes there it lights up)
+    // hover:shadow-lg : hover the shadow..
+  return (
+    <>
+        <div className='h-96'>
+            Hello
+        </div>
+    </>
+  )
+}
+
+export default TwClasses

465b47c325a014437635cc2fab1de7f0cd2c66a2
SiriSamala <sirisamala09@gmail.com>
2024-11-12 22:04:35 +0530

    Day15(Add product integration)

--- Express/server-mongoose/routes/UserRoute.js
+++ Express/server-mongoose/routes/UserRoute.js
@@ -12,11 +12,9 @@ router.get('/all', async (req, res) => {
 })
 
 router.post('/add', async (req, res) => {
-
-
     try {
         // const newuser = new Users(req.body)
-        const { name, email, phone, password,role } = req.body
+        const { name, email, phone, password, role } = req.body
         if (!name || !email || !phone || !password || !role) {
             return res.status(401).json({ message: "All fields required" })
         }
@@ -24,18 +22,18 @@ router.post('/add', async (req, res) => {
         //TODO : Add User Email & Phone Validation
 
         //Email
-        const exisitingemail = await Users.findOne({email})
-        if(exisitingemail) {
+        const exisitingemail = await Users.findOne({ email })
+        if (exisitingemail) {
             return res.status(500).json({ message: `User with ${email} already exists !` })
         }
 
         //Phone
-        const exisitingphone = await Users.findOne({phone})
-        if(exisitingphone) {
+        const exisitingphone = await Users.findOne({ phone })
+        if (exisitingphone) {
             return res.status(500).json({ message: `User with ${phone} already exists !` })
         }
         const salt = await bcrypt.genSalt(10)
-        const hashedpassword = await bcrypt.hash(password,salt)
+        const hashedpassword = await bcrypt.hash(password, salt)
         const newuser = new Users({
             name,
             email,
@@ -48,24 +46,6 @@ router.post('/add', async (req, res) => {
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
-    // try {
-    //     const newuser = new Users(req.body)
-    //     const { name, email, phone, password } = newuser
-    //     if (!name || !email || !phone || !password) {
-    //         res.status(400).json({ message: "All fields required" })
-    //     }
-
-    //     //TODO : Add User Email & Phone Validation
-
-    //     //Email
-
-    //     //Phone
-
-    //     await newuser.save()
-    //     res.status(200).json(newuser)
-    // } catch (error) {
-    //     res.status(500).json({ message: error.message })
-    // }
 })
 
 router.put('/edit/:id', async (req, res) => {
--- Project/store-client/src/components/Admin/AdminPageHeader.jsx
+++ Project/store-client/src/components/Admin/AdminPageHeader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AdminPageHeader = ({title}) => {
   return (
     <>
-        <div className='  h-full w-full flex flex-col justify-start items-start text-3xl font-bold text-purple-500'>
+        <div className='  h-10 w-95% flex flex-col justify-start items-start text-3xl font-bold text-purple-500'>
             {title}
         </div>
     </>

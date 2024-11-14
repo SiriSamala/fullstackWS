4786d5007dd2336957fea2b6145e3d429b42c147
SiriSamala <sirisamala09@gmail.com>
2024-11-14 21:46:33 +0530

    Day17 (ResetPassword endpoint added)

--- Express/server-mongoose/routes/UserRoute.js
+++ Express/server-mongoose/routes/UserRoute.js
@@ -5,6 +5,7 @@ const Users = require('../models/UsersModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+
 router.get('/count',async(req,res)=>{
     try{
         const count =await Users.countDocuments()
@@ -37,6 +38,7 @@ router.post('/add', async (req, res) => {
         const exisitingemail = await Users.findOne({ email })
         if (exisitingemail) {
             return res.status(409).json({ message: `User with ${email} already exists !` })
+            
         }
 
         //Phone
@@ -69,6 +71,26 @@ router.put('/edit/:id', async (req, res) => {
         }
         const updateduser = await Users.findByIdAndUpdate(id, req.body, { new: true })
         return res.status(200).json(updateduser)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+})
+
+router.put('/resetpassword/:id',async(req,res)=>{
+    try {
+        const id = req.params.id
+        const {password}=req.body
+        if(!password){
+             return res.status(400).json("Invalid request")
+        }
+        const existinguser = await Users.findOne({ _id: id })
+        if (!existinguser) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedpassword = await bcrypt.hash(password, salt)
+        const updateduser = await Users.findByIdAndUpdate(id, {password:hashedpassword} , { new: true })
+        return res.status(200).json({message :'Password updated'})
     } catch (error) {
         return res.status(500).json({ message: error.message })
     }
